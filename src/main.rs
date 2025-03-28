3ac1fc13b179d99aae04cd50f75f7ca852733336
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2025-03-28 15:52:42 -0300

    starting mlua

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use mlua::Lua;
+
+fn main() -> mlua::Result<()> {
+    // Cria um novo ambiente Lua
+    let lua = Lua::new();
+
+    // Script Lua como string
+    let script = r#"
+        function add(a, b)
+            return a + b
+        end
+
+        return add(3, 4)
+    "#;
+
+    // Executa o script e pega o retorno
+    let result: i32 = lua.load(script).eval()?;
+    println!("Resultado: {}", result);
+
+    Ok(())
 }
